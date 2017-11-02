29d511c3a1f7d22d40a6ebd388fcb303d6944e9a
Student <Student@PC97>
2017-11-02 14:25:23 +0800

    Wi Are Wi Wi Are

---
+++ src/freakinghell/FreakingHell.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package freakinghell;
+
+/**
+ *
+ * @author Student
+ */
+public class FreakingHell {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//        for(int i=0; i<100; i++){
+//        System.out.println("Fuck you +"+ i+ "!!");
+//        }
+        int i=0;
+        while(i<100){
+            System.out.println("Fuck you +"+ i+ "!!");
+            i++;
+        }
+    }
+    
+}
